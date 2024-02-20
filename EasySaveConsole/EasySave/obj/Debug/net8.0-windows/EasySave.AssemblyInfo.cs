49b5424cdcf7249ddc2688023e9ac9f8b5a93dd3
Aymar <euphrem0427@gmail.com>
2024-02-20 10:42:57 +0100

    Version 2.0

--- EasySaveConsole/EasySave/obj/Debug/net8.0-windows/EasySave.AssemblyInfo.cs
+++ EasySaveConsole/EasySave/obj/Debug/net8.0-windows/EasySave.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4d47dc919749d483433f033ee90bc5d88940a26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4e18569aa00a02e860c7e45a968edc13cec934")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
